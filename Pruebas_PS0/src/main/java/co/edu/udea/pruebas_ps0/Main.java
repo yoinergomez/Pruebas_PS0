8d4d59e7198f0e92ead9925bf9c7318d80fd2e7a
yoiner.gomez <yoiner.gomez22@gmail.com>
2017-08-11 12:14:04 -0500

    Agregando clase principal Main.java

---
+++ Pruebas_PS0/src/main/java/co/edu/udea/pruebas_ps0/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.udea.pruebas_ps0;
+
+/**
+ * Es la clase principal que se encarga de ejecutar el proyecto.
+ * @author Yoiner Gómez - yoiner.gomez22@gmail.com
+ * @date 2017/08/11
+ * @version v1
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello world!!!");
+    }
+    
+}
