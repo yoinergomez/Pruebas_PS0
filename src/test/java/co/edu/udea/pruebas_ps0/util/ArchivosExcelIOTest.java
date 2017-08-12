3f51f95e4f1d9765990258ef7588f690a78b9c2e
jhonatorozco <jhonatorozco@gmail.com>
2017-08-12 00:27:47 -0500

    Casos de prueba para abrir un archivo de Excel

--- src/test/java/co/edu/udea/pruebas_ps0/util/ArchivosExcelIOTest.java
+++ src/test/java/co/edu/udea/pruebas_ps0/util/ArchivosExcelIOTest.java
@@ -9,6 +9,9 @@ import co.edu.udea.pruebas_ps0.ldl.LDL;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.poi.ss.usermodel.Sheet;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -21,34 +24,86 @@ import static org.junit.Assert.*;
  * @author Pc1
  */
 public class ArchivosExcelIOTest {
-    
+
     public ArchivosExcelIOTest() {
-    }
-    
-    @Test
-    public void existeArchivosExcelIO(){
-        ArchivosExcelIO archivosExcelIO=new ArchivosExcelIO();
-        assertNotNull(archivosExcelIO);
-    
     }
 
     /**
-     * Test of convertirExcelALDL method, of class ArchivosExcelIO.
+     *
+     */
+    @Test
+    public void testExisteArchivosExcelIO() {
+        ArchivosExcelIO archivosExcelIO = new ArchivosExcelIO();
+        assertNotNull(archivosExcelIO);
+
+    }
+
+    /**
+     * Test of testAbrirArchivoInexistente method, of class ArchivosExcelIO.
+     *
      * @throws java.io.FileNotFoundException
      */
-    @Test(expected=FileNotFoundException.class)
-    public void testAbrirArchivoExcel() throws FileNotFoundException {
-        ArchivosExcelIO archivosExcelIO=new ArchivosExcelIO();
-        String nombreArchivo="prueba.txt";
-        File f= archivosExcelIO.abrirArchivoExcel(nombreArchivo);
+    @Test(expected = FileNotFoundException.class)
+    public void testAbrirArchivoInexistente() throws FileNotFoundException, Exception {
+        ArchivosExcelIO archivosExcelIO = new ArchivosExcelIO();
+        String nombreArchivo = "prueba2.txt";
+        File f = archivosExcelIO.abrirArchivoExcel(nombreArchivo);
+    }
+
+    /**
+     *
+     * @throws FileNotFoundException
+     */
+    @Test
+    public void testAbrirArchivoExistente() throws FileNotFoundException, Exception {
+        ArchivosExcelIO archivosExcelIO = new ArchivosExcelIO();
+        String path = this.getClass().getClassLoader().getResource("prueba.xls")
+                .toURI().toString();
+
+        path=path.substring(6);
+        File f = archivosExcelIO.abrirArchivoExcel(path);
+        assertTrue(f.exists());
     }
 
     /**
      * Test of escribirEnExcel method, of class ArchivosExcelIO.
+     *
+     * @throws java.io.FileNotFoundException
      */
     @Test
-    public void testEscribirEnExcel() {
+    public void testAbrirArchivosExcel() throws FileNotFoundException, Exception {
+         ArchivosExcelIO archivosExcelIO = new ArchivosExcelIO();
+        String path = this.getClass().getClassLoader().getResource("prueba.xls")
+                .toURI().toString();
+
+        path=path.substring(6);
+        File f = archivosExcelIO.abrirArchivoExcel(path);
+        String ext = FilenameUtils.getExtension(f.getName());
+        assertArrayEquals("xls".toCharArray(), ext.toCharArray());
+    }
+
+    /**
+     *
+     * @throws FileNotFoundException
+     * @throws Exception
+     */
+    @Test(expected = Exception.class)
+    public void testAbrirArchivoDistintoExcel() throws FileNotFoundException, Exception {
+      ArchivosExcelIO archivosExcelIO = new ArchivosExcelIO();
+        String path = this.getClass().getClassLoader().getResource("prueba.txt")
+                .toURI().toString();
+
+        path=path.substring(6);
+        File f = archivosExcelIO.abrirArchivoExcel(path);
+    }
+
+    @Test
+    public void testobtenerHojaExcel() throws FileNotFoundException, Exception {
+//        ArchivosExcelIO archivosExcelIO=new ArchivosExcelIO();
+//        String nombreArchivo="C:/Users/Pc1/Desktop1/prueba.xls";
+//        archivosExcelIO.convertirExcelALDL(nombreArchivo);
+//        Sheet sheet= archivosExcelIO.
 
     }
-    
+
 }
